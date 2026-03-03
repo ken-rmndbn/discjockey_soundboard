2a1fb6b5a41174e8f655c5680d5f829d004e8836
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-03 19:35:38 +0800

    window labelling

--- main_program.py
+++ main_program.py
@@ -9,12 +9,12 @@ windows.title("DJ Soundboard")
 windows.geometry("500x520")
 windows.configure(bg="1e1e1e")
 
-sound_list = {"Laugh": "laugh_sound.mp3",
-              "Sad": "sad_sound.mp3",
-              "Applause": "applause_sound.mp3",
-              "Call": "call_sound.mp3",
-              "New Year": "newyear_sound.mp3"
-            }
+sounds = {"Laugh": "laugh_sound.mp3",
+          "Sad": "sad_sound.mp3",
+          "Applause": "applause_sound.mp3",
+          "Call": "call_sound.mp3",
+          "New Year": "newyear_sound.mp3"
+         }
 
 def play_sound(name, file):
     try:
@@ -34,4 +34,14 @@ def change_volume(value):
     pygame.mixer.music.set_volume(float(value))
     volume_value_label.config(text=f"{int(float(value)*100)}%")
 
+title = tk.Label(window,
+                 text = "SOUNDBOARD PAD",
+                 font =("Arial", 18, "bold"),
+                 bg ="1e1e1e",
+                 fg = "white")
+title.pack(pady = 20)
 
+grid_frame = tk.Frame(window, bg = "1e1e1e")
+grid_frame.pack()
+
+sound_list = list(sounds.items())
