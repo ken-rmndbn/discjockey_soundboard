9b4801a5d44c5d31c4e44865ebaed353c8c40e58
ken-rmndbn <remandabankenneth29@gmail.com>
2026-03-03 18:38:41 +0800

    Created window for my soundboard

--- main_program.py
+++ main_program.py
@@ -2,4 +2,17 @@ import tkinter as tk
 from tkinter import messagebox
 import pygame
 
+pygame.mixer.init()
+
+windows = tk.Tk()
+windows.title("DJ Soundboard")
+windows.geometry("500x520")
+windows.configure(bg="1e1e1e")
+
+sound_list = {"Laugh": "laugh_sound.mp3",
+              "Sad": "sad_sound.mp3",
+              "Applause": "applause_sound.mp3",
+              "Call": "call_sound.mp3",
+              "New Year": "newyear_sound.mp3"
+              }
 
